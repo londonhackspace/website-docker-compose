3ca27800cc2cacfab08311a64c02ba97cc591492
Michael Cullen <michael@cullen-online.com>
2020-03-05 02:12:23 +0000

    Updated to new version

--- django/run.sh
+++ django/run.sh
@@ -14,11 +14,11 @@ echo "db:*:hackspace:hackspace:hackspace" > ~/.pgpass
 chmod 600 ~/.pgpass
 
 if [ ! -f /data/scratch/init ]; then
+	psql < /var/www/hackspace-foundation-sites/etc/create-flourish-tables.sql
 	~/venv/bin/python manage.py migrate sites
 	~/venv/bin/python manage.py migrate
 	~/venv/bin/python manage.py loaddata main/fixtures/*
 
-	psql < /var/www/hackspace-foundation-sites/etc/create-flourish-tables.sql
 	psql < /var/www/hackspace-foundation-sites/etc/restore-column-defaults.sql
 	psql < /var/www/hackspace-foundation-sites/etc/restore-multicolumn-pks.sql
 
@@ -27,6 +27,6 @@ fi
 
 ~/venv/bin/python manage.py migrate
 
-~/venv/bin/python manage.py collectstatic
+~/venv/bin/python manage.py collectstatic --noinput
 
 ~/venv/bin/python manage.py runserver 0.0.0.0:9001
